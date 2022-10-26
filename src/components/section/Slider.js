30987b5fd65b289a4d1d1f8ad98831571782909d
kimyihyung <kimlh3743@gmail.com>
2022-10-26 12:53:55 +0900

    2022.10.26

--- src/components/section/Slider.js
+++ src/components/section/Slider.js
@@ -8,60 +8,74 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/autoplay";
 function Slider() {
-    return (
-        <div className="slider__inner">
-            <Swiper
-                autoplay={{
-                    delay: 3000,
-                    disableOnInteraction: false,
-                }}
-                pagination={{ clickable: true }}
-                navigation={true}
-                modules={[Navigation, Pagination, Autoplay]}
-                className="mySwiper"
-            >
-                <SwiperSlide>
-                    <div class="desc">
-                        <span>WEATHER</span>
-                        <h3>Weather</h3>
-                        <p>기상 정보를 알고있는 것은 중요합니다.<br />
-                            날씨에 대한 정보를 알아봅시다.
-                        </p>
-                        <div class="btn">
-                            <a href="/">자세히 보기</a>
-                            <a href="/" class="black">사이트 보기</a>
-                        </div>
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div class="desc">
-                        <span>WEATHER</span>
-                        <h3>Weather</h3>
-                        <p>기상 정보를 알고있는 것은 중요합니다.<br />
-                            날씨에 대한 정보를 알아봅시다.
-                        </p>
-                        <div class="btn">
-                            <a href="/">자세히 보기</a>
-                            <a href="/" class="black">사이트 보기</a>
-                        </div>
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div class="desc">
-                        <span>WEATHER</span>
-                        <h3>Weather</h3>
-                        <p>기상 정보를 알고있는 것은 중요합니다.<br />
-                            날씨에 대한 정보를 알아봅시다.
-                        </p>
-                        <div class="btn">
-                            <a href="/">자세히 보기</a>
-                            <a href="/" class="black">사이트 보기</a>
-                        </div>
-                    </div>
-                </SwiperSlide>
-            </Swiper>
-        </div >
-    );
+  return (
+    <section id="sliderType" className={`slider__wrap ${props.fonts}`}>
+      <div className="slider__inner">
+        <Swiper
+          autoplay={{
+            delay: 3000,
+            disableOnInteraction: false,
+          }}
+          pagination={{ clickable: true }}
+          navigation={true}
+          modules={[Navigation, Pagination, Autoplay]}
+          className="mySwiper"
+        >
+          <SwiperSlide>
+            <div class="desc">
+              <span>WEATHER</span>
+              <h3>Weather</h3>
+              <p>
+                기상 정보를 알고있는 것은 중요합니다.
+                <br />
+                날씨에 대한 정보를 알아봅시다.
+              </p>
+              <div class="btn">
+                <a href="/">자세히 보기</a>
+                <a href="/" class="black">
+                  사이트 보기
+                </a>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div class="desc">
+              <span>WEATHER</span>
+              <h3>Weather</h3>
+              <p>
+                기상 정보를 알고있는 것은 중요합니다.
+                <br />
+                날씨에 대한 정보를 알아봅시다.
+              </p>
+              <div class="btn">
+                <a href="/">자세히 보기</a>
+                <a href="/" class="black">
+                  사이트 보기
+                </a>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div class="desc">
+              <span>WEATHER</span>
+              <h3>Weather</h3>
+              <p>
+                기상 정보를 알고있는 것은 중요합니다.
+                <br />
+                날씨에 대한 정보를 알아봅시다.
+              </p>
+              <div class="btn">
+                <a href="/">자세히 보기</a>
+                <a href="/" class="black">
+                  사이트 보기
+                </a>
+              </div>
+            </div>
+          </SwiperSlide>
+        </Swiper>
+      </div>
+    </section>
+  );
 }
 
 export default Slider;
